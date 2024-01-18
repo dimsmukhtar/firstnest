c6207ce2cc6a2790e6b641b1f6f4ccf3a6de8d0a
Dimas-Mukhtar <95218465+Dimas-Mukhtar@users.noreply.github.com>
2024-01-19 00:19:25 +0700

    add authentication protec route with guards

--- src/auth/strategy/jwt.srategy.ts
+++ src/auth/strategy/jwt.srategy.ts
@@ -4,11 +4,16 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
   constructor(config: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
     });
+  }
+
+  validate(payload: any) {
+    // jadi jwt strategy ini kaya kita bikin middleare authenticate di express jadi akan buat req.user. kalau endpoint nge hit api yang udah useGuard maka akan otomatis nge run finction validate ini,
+    return payload; // kalau di set misal return "hi", maka jika kita console.log dari controller req.user nya maka datanya hanya hi
   }
 }
