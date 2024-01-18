c66bc0789095912e9436848b0088dcafbb611daa
Dimas-Mukhtar <95218465+Dimas-Mukhtar@users.noreply.github.com>
2024-01-18 23:12:00 +0700

    add nest js config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
-import { Module } from "@nestjs/common"
-import { AuthModule } from "./auth/auth.module"
+import { ConfigModule } from '@nestjs/config';
+import { Module } from '@nestjs/common';
+import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { BookmarkModule } from './bookmark/bookmark.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, UserModule, BookmarkModule, PrismaModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AuthModule,
+    UserModule,
+    BookmarkModule,
+    PrismaModule,
+  ], // ConfogModule.forRoot({}) dengan ini .env file berfungsi
 })
 export class AppModule {}
--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,15 +1,16 @@
-import { Injectable } from "@nestjs/common"
-import { PrismaClient } from "@prisma/client"
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient {
-  constructor() {
+  constructor(config: ConfigService) {
     super({
       datasources: {
         db: {
-          url: "postgresql://postgres:postgress@localhost:5432/learn-nestjs",
+          url: config.get('DATABASE_URL'),
         },
       },
-    })
+    });
   }
 }
